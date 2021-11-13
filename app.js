598320ef3579ffd821a1a952841f7226d07e5dc2
urdogan0000 <hydrurdgn@gmail.com>
2021-11-13 14:43:13 +0300

    Ejs added

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
+//template engine
+app.set('view engine','ejs')
+
+
+//Middlewares
+app.use(express.static('public'))
+
+//Routes
+
 app.get("/", (req, res) => {
-  res.status(200).send("Anasayfa");
+  res.status(200).render('index',{
+      page_name:'index'
+  })
+});
+app.get("/about", (req, res) => {
+  res.status(200).render('about',{
+    page_name:'about'})
 });
 
 const PORT = 3000;
