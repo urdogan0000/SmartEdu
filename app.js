5dcb00b28c1f2e0d65203366977e6f6ce9c48095
urdogan0000 <hydrurdgn@gmail.com>
2021-11-14 16:20:00 +0300

    Course Page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const pageRoute = require("./routes/pageRoute");
+const courseRoute=require('./routes/courseRoute')
 
 const app = express();
 
@@ -8,25 +9,27 @@ const app = express();
 app.set("view engine", "ejs");
 
 //Connect DB
+var uri =
+  "mongodb://user:asd123@cluster0-shard-00-00.yuseg.mongodb.net:27017,cluster0-shard-00-01.yuseg.mongodb.net:27017,cluster0-shard-00-02.yuseg.mongodb.net:27017/SmartEdudB?ssl=true&replicaSet=atlas-9lchyf-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose
-  .connect(
-    "mongodb+srv://admin:asd123@cluster0.9vwzc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-     
-     
-    }
-  )
+  .connect(uri, {
+    useNewUrlParser: true,
+  })
   .then(() => {
     console.log("Db Connected Succesfully");
+  }).catch(()=>{
+    console.log('Veritabnına bağlanırken hata')
   });
 
 //Middlewares
 app.use(express.static("public"));
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 //Routes
 
 app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 
 const PORT = 3000;
 
