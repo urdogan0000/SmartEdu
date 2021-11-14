9a707ce0d3e3503c439f133c7828fd3d50d637cc
urdogan0000 <hydrurdgn@gmail.com>
2021-11-14 13:17:23 +0300

    as

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const pageRoute = require("./routes/pageRoute");
+
 const app = express();
 
 //template engine
-app.set('view engine','ejs')
+app.set("view engine", "ejs");
 
+//Connect DB
+mongoose
+  .connect(
+    "mongodb+srv://admin:asd123@cluster0.9vwzc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+     
+     
+    }
+  )
+  .then(() => {
+    console.log("Db Connected Succesfully");
+  });
 
 //Middlewares
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //Routes
 
-app.get("/", (req, res) => {
-  res.status(200).render('index',{
-      page_name:'index'
-  })
-});
-app.get("/about", (req, res) => {
-  res.status(200).render('about',{
-    page_name:'about'})
-});
+app.use("/", pageRoute);
 
 const PORT = 3000;
 
